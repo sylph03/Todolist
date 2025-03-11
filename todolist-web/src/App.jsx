c25abd8c5f132c65f5befbb9d137b901432e7ca1
sylph03 <sylph203@gmail.com>
2025-03-11 09:49:12 +0700

    update front roboto and icons lucide

--- todolist-web/src/App.jsx
+++ todolist-web/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Heart } from 'lucide-react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,9 +25,10 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs mb-4 font-bold">
         Click on the Vite and React logos to learn more
       </p>
+      <Heart color="red" size={24}/>
     </>
   )
 }
