f7048b4c02fa5ab65abb96db28aa81d423463a74
sylph03 <sylph203@gmail.com>
2025-03-27 17:12:58 +0700

    init todolist-api

---
+++ todolist-api/src/server.js
@@ -0,0 +1,15 @@
+// const express = require('express')
+import express from 'express'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world!</h1>')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`server is running http://${hostname}:${port}`)
+})
