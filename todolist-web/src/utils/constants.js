a08e46a31f0ad3619ab8e28cab2f5d500ce9c414
sylph03 <sylph203@gmail.com>
2025-04-26 21:47:38 +0700

    Deloy Production (Vercel)

--- todolist-web/src/utils/constants.js
+++ todolist-web/src/utils/constants.js
@@ -1,2 +1,18 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://todolist-api-4ntb.onrender.com'
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+let apiRoot = ''
+
+// console.log(import.meta.env)
+console.log(process.env)
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://todolist-api-4ntb.onrender.com'
+}
+
+console.log('apiRoot', apiRoot)
+
+export const API_ROOT = apiRoot
--- todolist-web/vite.config.js
+++ todolist-web/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  define: {
+    // eslint-disable-next-line no-undef
+    'process.env': process.env
+  },
   plugins: [
     react(),
     tailwindcss()
