e80eff0f5998077e3c9477b9be3bc9d357f95951
sylph03 <sylph203@gmail.com>
2025-04-26 21:54:57 +0700

    Fix domain deloy

--- todolist-api/src/utils/constants.js
+++ todolist-api/src/utils/constants.js
@@ -1,6 +1,6 @@
 // Domains được phép truy cập từ cors
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' Không cần localhost nữa vì file config/cors đã luôn cho phép môi trường dev
-  'https://todolist-web-gamma.vercel.app/'
+  'https://todolist-web-gamma.vercel.app'
   // ...
 ]
--- todolist-web/vite.config.js
+++ todolist-web/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // Cho phép Vite sử dụng được process.env
   define: {
     // eslint-disable-next-line no-undef
     'process.env': process.env
